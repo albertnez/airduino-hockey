a449ff5cb2e0408dd0ce3b4998c44ca1436f288f
albertnez <albertmg7@gmail.com>
2016-01-23 18:26:13 +0100

    First commit

---
+++ Assets/Camera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public GameObject player;
+	private float velocityx = 0;
+	private float velocityy = 0;
+	private float velocityz = 0;
+	private float smoothTime = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 originalRotation = transform.rotation.eulerAngles;
+		Transform original = transform;
+		transform.LookAt (player.transform.position);
+		Vector3 targetRotation = transform.rotation.eulerAngles;
+		
+		transform.TransformDirection(originalRotation);
+		float elapsedRotationx = Mathf.SmoothDampAngle(originalRotation.x,targetRotation.x,ref velocityx,smoothTime);
+		float elapsedRotationy = Mathf.SmoothDampAngle(originalRotation.y,targetRotation.y,ref velocityy,smoothTime);
+		float elapsedRotationz = Mathf.SmoothDampAngle(originalRotation.z,targetRotation.z,ref velocityz,smoothTime);
+		//transform.rotation.SetLookRotation(new Vector3(elapsedRotationx,elapsedRotationy,elapsedRotationz));
+	}
+}
---
+++ Assets/handle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class handle : MonoBehaviour {
+
+	public float speed = 10.0f;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float nx = 0.0f;
+		if (Input.GetAxis ("Vertical") > 0) {
+			nx = -speed;
+		}
+		else if (Input.GetAxis ("Vertical") < 0) {
+			nx = speed;
+		}
+		float nz = 0.0f;
+		if (Input.GetAxis ("Horizontal") < 0) {
+			nz = -speed;
+		}
+		else if (Input.GetAxis ("Horizontal") > 0) {
+			nz = speed;
+		}
+		rb.velocity = new Vector3(nx, rb.velocity.y, nz);
+		//rb.velocity = new Vector3(10.0f, 0.0f, 0.0f);
+	}
+}
