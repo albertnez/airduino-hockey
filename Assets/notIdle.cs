f7bc0134a30091b6d503698c82e8d39cdd5fa5e9
Gonzalo Diez <pinkiibiza@gmail.com>
2016-01-24 00:06:15 +0100

    tirururituriru

---
+++ Assets/notIdle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class notIdle : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < 0)
+			GetComponent<Rigidbody>().AddForce(-0.1f,0,0);
+		else
+			GetComponent<Rigidbody>().AddForce(0.1f,0,0);
+			
+	}
+}
