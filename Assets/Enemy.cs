65e566371694744eb17352907f2717fd199851dc
albertnez <albertmg7@gmail.com>
2016-01-23 21:02:17 +0100

    Added enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float maxSpeed = 10.0f;
+	public GameObject disk;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float target = disk.transform.position.z - transform.position.z;
+		rb.velocity = new Vector3(0.0f, 0.0f, Mathf.Min(maxSpeed, target * speed));
+	}
+}
