5726e01390c1f798d6c1aa7a357a265bb13e7f0f
Gonzalo Diez <pinkiibiza@gmail.com>
2016-01-23 19:15:24 +0100

    Velocity is real

---
+++ Assets/ArmColliderVelocity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmColliderVelocity : MonoBehaviour {
+
+	public GameObject thingToMirror;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody>().velocity = (thingToMirror.transform.position - transform.position)/Time.fixedDeltaTime;
+
+	}
+}
