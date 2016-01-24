08782b934f025435937075421fab3882a7b418db
albertnez <albertmg7@gmail.com>
2016-01-24 00:55:17 -0500

    Added new table with holes. Started goal score counter

---
+++ Assets/Goal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	// Whether the player that scored here is player 1 or 2.
+	public int playerScored;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
